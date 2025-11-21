3f57c0fd7a04f8bad092f8ad34112997eccbbf9a
gugadsf34 <gugadsf34@gmail.com>
2025-11-21 17:09:02 +0000

    progresso

--- projeto.py
+++ projeto.py
@@ -133,55 +133,109 @@ def t_error(t):
 
 lexer = lex.lex()
 
+#YACC
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+)
 
 start = 'program'
 
 def p_program(p):
     'program : PROGRAM ID PONTO_VIRGULA resto'
+    p[0] = ('PROGRAM', p[2], p[4])
     print(f"Programa valido")
 
 def p_resto(p):
     'resto : vars BEGIN codigo END PONTO'
     print("Resto valido")
+    
+    p[0] = p[2]
 
 def p_vars(p):
     '''vars : VAR var_list
             | empty'''
     print("Vars valido")
+    if len(p)>2:
+        p[0]=p[2]
+    else:
+        p[0]=[]
 
 def p_var_list(p):
     '''var_list : var
                 | var_list var'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[2]]
 
 def p_var(p):
     'var : id_list DOIS_PONTOS tipo PONTO_VIRGULA'
+    p[0] = ('VARS', p[1], p[3])
 
 def p_id_list(p):
     '''id_list : ID
                | id_list VIRGULA ID'''
+    if len(p) == 2:
+        p[0]=p[1] 
+    else:
+        p[0]=p[1] + [p[3]]
     
 def p_tipo(p):
     '''tipo : INTEGER
             | STRING
             | BOOLEAN'''
+    p[0] = p[1]
     
 def p_codigo(p):
     '''codigo: comando_lista
-             | comando_lista PONTO_VIRGULA codigo'''
+             | codigo PONTO_VIRGULA comando_lista'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1]+[p[3]]
 
 def p_comando_lista(p):
-    '''comando_lista : comando
-                     | comando_atribuicao
+    '''comando_lista : comando_atribuicao
                      | comando_while
                      | comando_for
                      | comando_if
-                     | comando_readln
                      | comando_writeln
+                     | comando_readln
                      | comando_begin
                      | empty'''
+    p[0] = p[1]
     
 def p_comando_atribuicao(p):
     'comando_atribuicao : ID ASSIGN expressao'
+    p[0] = ('ASSIGN', p[1], p[3])
+
+def p_while(p):
+    'comando_while : WHILE expressao DO comando_lista'
+    p[0] = ('WHILE', p[2], p[4])
+
+def p_comando_for(p):
+    'comando_for : FOR ID ASSIGN expressao TO expressao DO comando_lista'
+    p[0] = ('FOR', p[2], p[4], p[6], p[8])
+    #falta downto
+
+def p_if(p):
+    '''comando_if : IF expressao THEN comando_lista'''
+    p[0] = ('IF', p[2], p[4])
+    
+def p_comando_writeln(p):
+    'comando_writeln : WRITELN PARENT_A argumentos PARENT_F' 
+    p[0] = ('WRITELN', p[3])
+
+def p_comando_readln(p):
+    'comando_readln : READLN PARENT_A ID PARENT_F'
+    p[0] = ('READLN', p[3])
+
+def p_begin(p):
+    'comando_begin : BEGIN codigo END'
+    p[0] = p[2]
+
 
 #falta mais tipo for e chamda de funcao
 def p_expressao(p):
@@ -190,36 +244,113 @@ def p_expressao(p):
                  | expressao MINUS termo
                  | expressao TIMES termo
                  | expressao DIVIDE termo
-                 | expressao BOOLEAN'''
+                 | expressao BOOL'''
+    p[0] = ('CONTA', p[2], p[1], p[3])
+
+def p_BOOL(p):
+    '''BOOL : TRUE
+            | FALSE'''  
+    p[0] = p[1]
 
 #NUM_REAL inutil? falta chamada de funcao
 def p_termo(p):
     '''termo : NUM_INT
              | NUM_REAL 
              | ID
+             | LIT_STRING
              | PARENT_A expressao PARENT_F'''
+    if p[1]=='(':
+        p[0]=p[2]
+    else:
+        p[0] = p[1]
 
-
-def p_comando_writeln(p):
-    'comando_writeln : WRITELN PARENT_A LIT_STRING PARENT_F PONTO_VIRGULA' 
-
-def p_comando_readln(p):
-    'comando_readln : READLN PARENT_A ID PARENT_F PONTO_VIRGULA'
-
-def p_comando_for(p):
-    'comando_for : FOR ID ASSIGN expressao TO expressao DO comando_atribuicao'
-    #falta downto
-
-
-
+def p_argumentos(p):
+    '''argumentos : expressao
+                  | argumentos VIRGULA expressao'''
+    if len(p) == 2:
+        p[0] = [p[1]]  
+    else: 
+        p[1] + [p[3]]
 
 
 
 def p_empty(p):
     'empty :'
     pass
+
+def p_error(p):
+    if p:
+        print(f"Erro de sintaxe: '{p.value}' na linha {p.lineno}")
+    else:
+        print("Erro de sintaxe: Fim inesperado do ficheiro")
     
 parser = yacc.yacc()
+
+#------------------------------------
+#-- Analisador Semântico (incompleto) ---
+
+tabela={}
+
+
+def analisador_semantico():
+    #talvez tirar
+    if not isinstance(nodo, tuple):
+        return
+
+    if isinstance(nodo, list):
+        for n in nodo:
+            anasalisador_semantico(n)
+        return
+    
+    caixa=nodo[0]
+
+    if caixa=='PROGRAM':
+        analisador_semantico(nodo[2])
+
+    elif caixa=='VARS':
+        vars_lista=nodo[1]
+        tipo=nodo[2]
+        for var in vars_lista:
+            if var in tabela:
+                print(f"Erro semântico: variável '{var}' já declarada.")
+        else:
+            tabela[var]=tipo
+            #debug tirar dps
+            print(f"Variável '{var}' declarada como '{tipo}'.")
+
+    elif caixa=='ASSIGN':
+        var=nodo[1]
+        expressao=nodo[2]
+        if var not in tabela:
+            print(f"Erro semântico: variável '{var}' não declarada.")
+        else:
+            analisador_semantico(expressao)
+
+
+
+
+
+
+
+
+
+
+#--
+def percorre(p):
+    etiqueta=p[0]
+    #if etiqueta=='PROGRAM':
+    if etiqueta=='VARS':
+        ids=p[1]
+        tipo=p[3]
+        if isinstance(ids, list):
+            for id in ids:
+                tabela[id]=tipo
+        else:
+            tabela[ids]=tipo
+
+
+
+
 
 if __name__ == '__main__':
     #codigo teste
